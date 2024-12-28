3623ff688a4768e5b83a3c339ec744c10b2f43f3
Sparsh Kandwal <sparshkandwal@outlook.com>
2024-12-28 15:22:06 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='sparsh',
+    author_email='sparshkandwal@outlook.com',
+    packages=find_packages()
+    
+    
+    
+)
